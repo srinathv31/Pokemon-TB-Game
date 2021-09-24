da195630b8691f99867007d9defa7861d0a4f0db
Srinath Venkatesh <srinathvb1@gmail.com>
2021-09-24 14:49:22 -0400

    9/24 Game Scene Set

--- pokemon-game/src/App.tsx
+++ pokemon-game/src/App.tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import logo from './assets/PokemonImages/logo.png';
 import './App.css';
+import DECK from './assets/pokemons.json';
+import { Pokemon } from './interfaces/pokemon';
 
-function App() {
+function App(): JSX.Element {
+  const [activeCard, setActiveCard] = useState<Pokemon>(DECK[0] as Pokemon);
+
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="cell cell-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit src/App.js and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,8 +23,20 @@ function App() {
           Learn React
         </a>
       </header>
+      {/* Player Hand */}
+      <aside className="cell cell-left">
+        <div>{DECK[0].name} {DECK[0].type}</div>
+        <div><img src={DECK[0].image} alt="pokemon" width="250"/></div>
+        <div>{DECK[0].attack}</div>
+        <div>{DECK[0].defense}</div>
+        <div>{DECK[0].health}</div>
+      </aside>
+      <main className="cell cell-main">Battle Stage</main>
+      <aside className="cell cell-right">Opponent Hand</aside>
+      <footer className="class cell-footer">Battle Log</footer>
     </div>
   );
 }
 
 export default App;
+
---
+++ pokemon-game/src/interfaces/pokemon.ts
@@ -0,0 +1,8 @@
+export interface Pokemon {
+    id: number
+    name: string
+    type: string
+    health: number
+    attack: number
+    defense: number
+}
